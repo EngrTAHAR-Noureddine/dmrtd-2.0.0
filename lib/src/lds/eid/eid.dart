dc7a56bf08b0b313e092948faffd69a2c7dbc331
TAHAR Noureddine <engr.tahar.noureddine@gmail.com>
2025-02-02 20:23:26 +0100

    Initial commit

---
+++ lib/src/lds/eid/eid.dart
@@ -0,0 +1,12 @@
+//  Created by Crt Vavros, copyright © 2022 ZeroPass. All rights reserved.
+import 'package:dmrtd/extensions.dart';
+
+// Class contains information Emirates ID application applet
+class EidDF {
+  // See: Section 3.1 https://www.icao.int/publications/Documents/9303_p10_cons_en.pdf
+  // ignore: non_constant_identifier_names
+  // static final AID  = "A00000024300130000000101".parseHex();
+  static final AID  = "A00000024300130000000101".parseHex();
+  // static final AID  = "A0000002471001".parseHex();
+  static const name = "Emirates ID Application";
+}
